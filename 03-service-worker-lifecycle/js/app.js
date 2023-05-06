5f6f4937b58d27f08bc99eb62b7fc4ed87179ba0
Alejo Boga <alejoboga20@gmail.com>
2023-05-06 15:16:21 -0400

    fetch event

--- 03-service-worker-lifecycle/js/app.js
+++ 03-service-worker-lifecycle/js/app.js
@@ -2,3 +2,7 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js');
 }
+
+fetch('https://reqres.in/api/users')
+	.then((response) => response.text())
+	.then((data) => console.log({ data }));
--- 03-service-worker-lifecycle/sw.js
+++ 03-service-worker-lifecycle/sw.js
@@ -7,7 +7,7 @@ self.addEventListener('install', (event) => {
 			console.log('Install done');
 			self.skipWaiting(); // use in development only
 			resolve();
-		}, 1000);
+		}, 1);
 	});
 
 	event.waitUntil(install);
@@ -16,4 +16,15 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
 	/* Deleted old caches */
 	console.log('Activating service worker...');
+});
+
+/* Fetch: Handling HTTP Requests */
+self.addEventListener('fetch', (event) => {
+	/* Handle the cache */
+	console.log(event.request.url);
+
+	if (event.request.url.includes('reqres.in')) {
+		const response = new Response(`{ok: false, message: 'Nope'}`);
+		event.respondWith(response);
+	}
 });
