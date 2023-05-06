485a82d0e4be93e7b8ff4394a38d26884c3dc40e
Alejo Boga <alejoboga20@gmail.com>
2023-05-06 10:55:49 -0400

    fetch event

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-console.log('SW: Hello World!');
+self.addEventListener('fetch', (event) => {
+	/* if (event.request.url.includes('style.css')) {
+		event.respondWith(null);
+    this is a way to block a request
+	} */
+	event.respondWith(fetch(event.request));
+});
