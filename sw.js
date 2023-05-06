b19da5df6b7fd5398ff5e706194ed45b3a4430cb
Alejo Boga <alejoboga20@gmail.com>
2023-05-06 11:30:05 -0400

    handle errors

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 self.addEventListener('fetch', (event) => {
-	if (event.request.url.includes('main.jpg')) {
-		const response = fetch('/img/main-turned.jpg');
+	const response = fetch(event.request).then((resp) => (resp.ok ? resp : fetch('/img/main.jpg')));
 
-		event.respondWith(response);
-	}
+	event.respondWith(response);
 });
