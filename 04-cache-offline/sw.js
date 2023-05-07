cb39f0f61471f5071a4eedee998bbad3d3269cc6
Alejo Boga <alejoboga20@gmail.com>
2023-05-07 14:45:48 -0400

    optimize caches

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,21 +1,22 @@
-const CACHE_NAME = 'cache-1';
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
 
 self.addEventListener('install', (e) => {
 	console.log('[Service Worker] - Installing...');
 
-	const cachePromise = caches.open(CACHE_NAME).then((cache) => {
+	const staticCachePromise = caches.open(STATIC_CACHE).then((cache) => {
 		/* Save app shell in cache */
-		return cache.addAll([
-			'/',
-			'index.html',
-			'css/style.css',
-			'img/main.jpg',
-			'js/app.js',
-			'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
-		]);
+		return cache.addAll(['/', 'index.html', 'css/style.css', 'img/main.jpg', 'js/app.js']);
 	});
 
-	e.waitUntil(cachePromise);
+	const inmutableCachePromise = caches
+		.open(INMUTABLE_CACHE)
+		.then((cache) =>
+			cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css')
+		);
+
+	e.waitUntil(Promise.all([staticCachePromise, inmutableCachePromise]));
 });
 
 /* Cache strategies */
@@ -29,7 +30,7 @@ self.addEventListener('fetch', (e) => {
 
 		return fetch(e.request).then((newResp) => {
 			/* Save new response in the cache */
-			caches.open(CACHE_NAME).then((cache) => cache.put(e.request, newResp));
+			caches.open(DYNAMIC_CACHE).then((cache) => cache.put(e.request, newResp));
 
 			return newResp.clone();
 		});
