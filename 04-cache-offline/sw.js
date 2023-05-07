09c6f4f580ba9a78065f24fa88ca72cd80f4283f
Alejo Boga <alejoboga20@gmail.com>
2023-05-07 10:46:07 -0400

    response offline

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,3 +1,9 @@
+self.addEventListener('fetch', (event) => {
+	const offlineResponse =
+		new Response(`Welcome to the offline page. You must be online to use the page
+  `);
 
+	const response = fetch(event.request).catch(() => offlineResponse);
 
-
+	event.respondWith(response);
+});
