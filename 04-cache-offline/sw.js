af784fc999dd9aabf766e3902e2e67948b2358c0
Alejo Boga <alejoboga20@gmail.com>
2023-05-08 13:40:12 -0400

    limit items in cache

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -2,6 +2,18 @@ const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
+const MAX_ITEMS_CACHE = 5;
+
+const cleanCache = (cacheName, sizeItems = MAX_ITEMS_CACHE) => {
+	caches.open(cacheName).then((cache) =>
+		cache.keys().then((keys) => {
+			if (keys.length >= sizeItems) {
+				cache.delete(keys[0]).then(() => cleanCache(cacheName, sizeItems));
+			}
+		})
+	);
+};
+
 self.addEventListener('install', (e) => {
 	console.log('[Service Worker] - Installing...');
 
@@ -30,7 +42,11 @@ self.addEventListener('fetch', (e) => {
 
 		return fetch(e.request).then((newResp) => {
 			/* Save new response in the cache */
-			caches.open(DYNAMIC_CACHE).then((cache) => cache.put(e.request, newResp));
+			caches.open(DYNAMIC_CACHE).then((cache) => {
+				console.log(e.request.url);
+				cache.put(e.request, newResp);
+				cleanCache(DYNAMIC_CACHE, MAX_ITEMS_CACHE);
+			});
 
 			return newResp.clone();
 		});
