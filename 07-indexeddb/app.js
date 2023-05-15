9a53cc1df675c6abf3996180d38e877239b893a9
Alejo Boga <alejoboga20@gmail.com>
2023-05-15 09:54:27 -0400

    init db

--- 07-indexeddb/app.js
+++ 07-indexeddb/app.js
@@ -1,1 +1,12 @@
 // indexedDB
+const DB_VERSION = 1;
+const request = window.indexedDB.open('my-database', DB_VERSION);
+
+/* It triggers when version is upgraded */
+request.onupgradeneeded = (event) => {
+	console.log('[IndexDB] - Update DB');
+
+	const db = event.target.result;
+
+	db.createObjectStore('heroes', { keyPath: 'id' });
+};
